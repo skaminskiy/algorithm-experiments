ed7623a0c5dafd30bccc6f66ccc3bf331a68c398
Sergii Kaminskyi <sergii.kaminskyi@globalrelay.net>
2017-04-13 10:15:11 -0700

    1

---
+++ src/org/ska/BitManipulation/Insertion.java
@@ -0,0 +1,26 @@
+package org.ska.BitManipulation;
+
+import java.util.BitSet;
+
+public class Insertion {
+     public static void main(String[] args) {
+            
+            System.out.println(Integer.toBinaryString(insert(0b10000000000, 0b10011, 2, 6))); //10001001100
+            System.out.println(Integer.toBinaryString(insert(0b10101110101, 0b10011, 2, 6))); //10101001101
+     }
+     
+     static int insert(int n, int m, int j, int i) {
+         
+         BitSet result = BitSet.valueOf(new long[]{(long)n});
+         
+         for (; j <= i; j++) {
+             result.clear(j);
+         }
+         
+         BitSet mSet = BitSet.valueOf(new long[]{(long)(m*4)});
+         
+         result.or(mSet);
+         
+         return (int)result.toLongArray()[0];
+     }
+}
--- src/org/ska/datastructures/StacksAndQueues/SetOfStacksRun.java
+++ src/org/ska/datastructures/StacksAndQueues/SetOfStacksRun.java
@@ -1,5 +1,9 @@
 package org.ska.datastructures.StacksAndQueues;
 
+import java.util.ArrayList;
+
+import org.ska.datastructures.LinkedLists.Node;
+
 public class SetOfStacksRun {
     public static void main(String[] args) {
         
@@ -12,6 +16,11 @@ public class SetOfStacksRun {
         stack.push(15);
         stack.push(7);
         stack.push(21);
+        
+//        System.out.println(stack.popAt(1)); //15
+//        System.out.println(stack.popAt(1)); //1
+//        System.out.println(stack.popAt(1)); //8
+//        System.out.println(stack.popAt(1)); //21
 
           System.out.println(stack.peek()); //21
           System.out.println(stack.isEmpty()); //false
@@ -29,40 +38,117 @@ public class SetOfStacksRun {
     static public class SetOfStacks<T> {
         
         private final int limit;
+        private final ArrayList<OneStack<T>> list = new ArrayList<OneStack<T>>();
         
         public SetOfStacks(int limit) {
            this.limit = limit;
         }
         
-        public void push(T item) {
+        private void shift(ArrayList<OneStack<T>> list, int stackIndex) {
+            OneStack<T> nextStack = null;
+            OneStack<T> stack = null;
             
+            if (list.size() > stackIndex + 1) {
+                nextStack = list.get(stackIndex+1);
+            }
+            if (list.size() > stackIndex) {
+                stack = list.get(stackIndex);
+            }
+            
+            if (stack == null) {
+                return;
+            }
+            
+            if (stack.size == 0) {
+                list.remove(stackIndex);
+                return;
+            }
+            
+            if (nextStack == null) {
+                return;
+            }
+            
+  
+            stack.last.next = nextStack.first;
+            stack.last = nextStack.first;
+            nextStack.first = nextStack.first.next;
+            stack.last.next = null;
+            stack.size++;
+            nextStack.size--;
+
+            shift(list, stackIndex+1);
+        }
+        
+        public void push(T item) {
+            Node<T> node = new Node<T>(item);
+            
+            if (list.size() > 0) {
+                OneStack<T> firstStack = list.get(list.size() - 1);
+                
+                if (firstStack.size < this.limit) {
+                    
+                    node.next = firstStack.first;
+                    firstStack.first = node;
+                    firstStack.size++;
+                    return;
+                }
+            }
+            
+            list.add(new OneStack<T>(node));
         }
         
         public T pop() {
-            return null;
+            return popAt(list.size() - 1);
         }
         
         public T peek() {
+            if (list.size() > 0) {
+                return list.get(list.size() - 1).first.data;
+            }
+            
             return null;
         }
        
         public boolean isEmpty() {
-            return false;
+            return list.size() == 0;
         }
         
         public T popAt(int index) {
-            return null;
+            if (list.size() < index+1) {
+                return null;
+            }
+            
+            OneStack<T> stack = list.get(index);
+            
+            Node<T> node = stack.first;
+            stack.first = node.next;
+            stack.size--;
+            shift(list, index);
+            
+            return node.data;
         }
         
         static class OneStack<T> {
             
-            private final int limit;
+            private Node<T> first = null;
+            private Node<T> last = null;
+            private int size;
             
-            public OneStack(int limit) {
-               this.limit = limit;
+            public OneStack(Node<T> first) {
+               this.first = first;
+               this.last = first;
+               this.size = 1;
             }
+        }
+        
+        static class Node<T> {
             
+          public Node<T> next;
+          private T data;
             
+          public Node(T data) {
+              this.data = data;
+          }
         }
     }
 }
